644388a3c03356e2e365b5949d58a45536b54b3b
termie <github@anarkystic.com>
2016-01-08 18:43:24 -0800

    change token warning

--- version.go
+++ version.go
@@ -14,7 +14,7 @@ var (
 	MajorVersion = "0"
 
 	// MinorVersion is the semver minor version.
-	MinorVersion = "3"
+	MinorVersion = "4"
 
 	// PatchVersion is the semver patch version. (use 0 for dev, build process
 	// will inject a build number)

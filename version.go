9173c94ee8f4f046c5c4092c33a84f7c328050ca
termie <github@anarkystic.com>
2016-01-08 17:15:19 -0800

    bump version

--- version.go
+++ version.go
@@ -14,7 +14,7 @@ var (
 	MajorVersion = "0"
 
 	// MinorVersion is the semver minor version.
-	MinorVersion = "1"
+	MinorVersion = "2"
 
 	// PatchVersion is the semver patch version. (use 0 for dev, build process
 	// will inject a build number)
